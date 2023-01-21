a57a8427a40134c853154a4007d4a030d0905ce7
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2023-01-21 14:37:59 +0000

    String reversal

--- AlgoCasts/exercises/reversestring/index.js
+++ AlgoCasts/exercises/reversestring/index.js
@@ -7,17 +7,21 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  let reversed = "";
-
-  for (let character of str) {
-    reversed = character + reversed;
-  }
-  return reversed;
+  return str.split("").reduce((acc, val) => val + acc, "");
 }
 
 module.exports = reverse;
 
+// Easy solution 1
 // One possible easy solution
 // function reverse(str) {
 //     return str.split("").reverse().join("");
 //   }
+
+// Medium solution 2
+// let reversed = "";
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
