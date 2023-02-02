88378f8f26b2603b7246b7c7e6a2730d191e1e74
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2023-02-02 08:06:21 +0000

    Adding Anagram Solution

--- AlgoCasts/exercises/anagrams/index.js
+++ AlgoCasts/exercises/anagrams/index.js
@@ -8,6 +8,46 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let compareStringA = stringA
+    .replace(/[^\w]/g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
+  let compareStringB = stringB
+    .replace(/[^\w]/g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
+
+  return compareStringA === compareStringB;
+}
 
 module.exports = anagrams;
+
+// Second verbose solution
+// const firstCharMap = BuildCharacterMap(stringA);
+//   const secondCharMap = BuildCharacterMap(stringB);
+
+//   if (Object.keys(firstCharMap).length !== Object.keys(secondCharMap).length) {
+//     return false;
+//   }
+
+//   for (let char in firstCharMap) {
+//     if (firstCharMap[char] !== secondCharMap[char]) {
+//       return false;
+//     }
+//     return true;
+//   }
+// }
+
+// function BuildCharacterMap(input) {
+//   let charMap = {};
+
+//   for (let char of input.replace("/[^w]g", "").toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   return charMap;
+// }
