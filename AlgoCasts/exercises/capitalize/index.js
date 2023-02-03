8962b47276d604ff2f4b61e2eed09b3b15d11ade
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2023-02-03 08:34:39 +0000

    Adding Capitalize first character of string

--- AlgoCasts/exercises/capitalize/index.js
+++ AlgoCasts/exercises/capitalize/index.js
@@ -7,6 +7,23 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  var collection = str.split(" ");
+  collection.forEach(function (element, index) {
+    collection[index] = element[0].toUpperCase() + element.slice(1);
+  }, collection);
+
+  return collection.join(" ");
+}
 
 module.exports = capitalize;
+
+// First Solution
+// var result = "";
+//   var stringCollection = str.split(" ");
+//   for (let item of stringCollection) {
+//     item = item[0].toUpperCase() + item.slice(1);
+//     result += item + " ";
+//   }
+
+//   return result.trimEnd();
