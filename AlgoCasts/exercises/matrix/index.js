9395007fd43ca6b13d4dce8cfa7e76b23340ec64
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2023-02-22 08:51:13 +0000

    Adding spiral array (Matrix)

--- AlgoCasts/exercises/matrix/index.js
+++ AlgoCasts/exercises/matrix/index.js
@@ -15,6 +15,52 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  // Creating an empty array
+  const matrix = [];
+
+  // Creating nested arrays and fill them with zeros
+  for (let i = 0; i < n; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < n; j++) {
+      matrix[i][j] = 0;
+    }
+  }
+
+  let num = 1;
+  let rowStart = (colStart = 0);
+  let rowEnd = (colEnd = n - 1);
+
+  while (rowStart <= rowEnd && colStart <= colEnd) {
+    // Populate first row
+    for (let i = colStart; i <= colEnd; i++) {
+      matrix[rowStart][i] = num++;
+    }
+    rowStart++;
+
+    // Populate right column
+    for (let i = rowStart; i <= rowEnd; i++) {
+      matrix[i][colEnd] = num++;
+    }
+    colEnd--;
+
+    // Populate bottom row
+    if (rowStart <= rowEnd) {
+      for (let i = colEnd; i >= colStart; i--) {
+        matrix[rowEnd][i] = num++;
+      }
+      rowEnd--;
+    }
+
+    // Populate left column
+    if (colStart <= colEnd) {
+      for (let i = rowEnd; i >= rowStart; i--) {
+        matrix[i][colStart] = num++;
+      }
+      colStart++;
+    }
+  }
+  return matrix;
+}
 
 module.exports = matrix;
