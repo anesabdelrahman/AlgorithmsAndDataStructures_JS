51298a6c5ae052d79aff20d1f22068144dd61b0d
Anes Abdelrahman <anes.abdelrahman@accesspay.com>
2023-03-20 08:59:07 +0000

    Implemnting queues using stacks

--- AlgoCasts/exercises/qfroms/index.js
+++ AlgoCasts/exercises/qfroms/index.js
@@ -12,8 +12,45 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.firstStack = new Stack();
+    this.secondStack = new Stack();
+  }
+
+  add(record) {
+    this.firstStack.push(record);
+  }
+
+  peek() {
+    this.reinstateFirstStacks(this.firstStack, this.secondStack);
+    const record = this.secondStack.peek();
+    this.reinstateSecondStacks();
+
+    return record;
+  }
+
+  remove() {
+    this.reinstateFirstStacks();
+    const record = this.secondStack.pop();
+    this.reinstateSecondStacks();
+
+    return record;
+  }
+
+  reinstateFirstStacks() {
+    while (this.firstStack.peek()) {
+      this.secondStack.push(this.firstStack.pop());
+    }
+  }
+
+  reinstateSecondStacks() {
+    while (this.secondStack.peek()) {
+      this.firstStack.push(this.secondStack.pop());
+    }
+  }
+}
 
 module.exports = Queue;
